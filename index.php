c7db879eb970725ab094fc69f4c9d7927dd540c0
datagutten <datagutten@datagutten.net>
2014-02-11 22:04:32 +0100

    Added title and some cleanup

--- index.php
+++ index.php
@@ -1,40 +1,27 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
-
-<body>
 <?Php
 require 'guild.php';
 $races=json_decode(file_get_contents("http://eu.battle.net/api/wow/data/character/races"),true);
 $classes=json_decode(file_get_contents("http://eu.battle.net/api/wow/data/character/classes"),true);
-
 $combos=json_decode(file_get_contents('combos.json'),true);
-//print_r($combos);
+
+$title="{$guild['name']} - {$guild['realm']} ({$guild['level']})";
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title><?Php echo $title; ?></title>
+</head>
+
+<body>
+<?Php
 echo "<table border=\"1\">\n";
-/*echo "\t<tr>\n";
-echo "\t  <td>&nbsp;</td>\n";*/
 
-/*foreach($races['races'] as $race)
-{
-	if($race['side']!='alliance')
-		continue;
-	echo "\t   <td>{$race['name']}</td>\n";
-
-}*/
-//echo "\t</tr>\n";
 array_unshift($classes['classes'],'header');
-//array_unshift($races['races'],'header');
 
 foreach($classes['classes'] as $classkey=>$class) //Class columns
 {
-
-	
 	echo "\t<tr>\n";
-	/*if(!is_array($class))
-		echo "\t   <td>start</td>\n";*/
 	foreach($races['races'] as $racekey=>$race) //Race rows
 	{
 
